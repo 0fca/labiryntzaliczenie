082af30b5615bcde97b7999e8ea22ee523be97cd
lukas <lukas@lukas>
2017-05-02 11:46:09 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/com/neology/eurotrackapp/datacontroller/Encrypter.java

---
+++ src/labiryntzaliczenie/LabiryntZaliczenie.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labiryntzaliczenie;
+
+import labiryntzaliczenie.gui.MainWindow;
+
+/**
+ *
+ * @author lukas
+ */
+public class LabiryntZaliczenie {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainWindow.main(args);
+    }
+    
+}
